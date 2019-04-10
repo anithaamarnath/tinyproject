bd5b705a038dca50ae2fc6170d38917152ec66e9
anithaamarnath <anithaamarnath04@gmail.com>
2019-04-10 02:33:04 +0000

    redirecting

--- express_server.js
+++ express_server.js
@@ -29,9 +29,17 @@ app.get("/urls/:shortURL", (req, res) => {
   //console.log(res.params.shortURL);
   res.render("urls_show", templateVars);
 });
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log(req.body);
+                           // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  //res.redirect(longURL);
+
+  res.status(301).redirect(longURL);
 });
 
 
